ae255f1c33317810eb34d0941a3e91acc9daaa79
RhineGST <GST49151709884@outlook.com>
2024-03-24 05:35:15 +0800

    add shoot jam back

--- include/shoot.hpp
+++ include/shoot.hpp
@@ -16,12 +16,16 @@ namespace Shoot
         void update_speed();
         void decomposition_speed();
         void control_loop();
+        bool isJam();
 
        public:
         bool no_force = true;
         bool friction_open = false;
         bool friction_finish = false;
         bool shoot_open = false;
+
+        int jam_time = 0;
+        int back_time = 0;
 
         std::vector<Hardware::Motor> friction;
         std::vector<Hardware::Motor> trigger;
