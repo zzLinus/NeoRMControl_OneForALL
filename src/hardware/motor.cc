c1c297b1c4c84873193471c708dcb426b15631c0
RhineGST <GST49151709884@outlook.com>
2024-03-23 14:31:28 +0800

    add get_frame, only apply on shoot

--- src/hardware/motor.cc
+++ src/hardware/motor.cc
@@ -2,15 +2,6 @@
 
 namespace Hardware
 {
-
-    //    template<>
-    //    Motor<Pid::Pid_position>::Motor(const Pid::Pid_config &config) : pid_ctrler(config) {
-    //    }
-    //
-    //    template<>
-    //    Motor<Pid::Pid_rad>::Motor(const Pid::Pid_config &config) : pid_ctrler(config) {
-    //    }
-
     void Motor::unpack(const can_frame& frame) {
         update_time();
         auto& motor_t = motor_measure;
@@ -19,5 +10,16 @@ namespace Hardware
         motor_t.speed_rpm = (uint16_t)(frame.data[2] << 8 | frame.data[3]);
         motor_t.given_current = (uint16_t)(frame.data[4] << 8 | frame.data[5]);
         motor_t.temperate = frame.data[6];
+    }
+
+    can_frame get_frame(canid_t can_id, const std::vector<Motor> &mot_list) {
+        can_frame frame{};
+        frame.can_id = can_id;
+        frame.can_dlc = 8;
+        for(int i = 0; i < mot_list.size() && i < 4; i++) {
+            frame.data[i << 1] = (mot_list[i].give_current >> 8);
+            frame.data[i << 1 | 1] = (mot_list[i].give_current & 0xFF);
+        }
+        return frame;
     }
 }  // namespace Hardware
