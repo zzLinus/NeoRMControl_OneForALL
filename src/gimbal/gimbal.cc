a1654b795a43c42f9bd0da03f6a61828237071fc
Zhang Zheng <2385747226@qq.com>
2024-03-25 03:12:26 +0800

    change gimbal motor channel configureation for sentry

--- src/gimbal/gimbal.cc
+++ src/gimbal/gimbal.cc
@@ -15,8 +15,8 @@ namespace Gimbal
     void Gimbal::init(const std::shared_ptr<Robot::Robot_set> &robot) {
         robot_set = robot;
 
-        Robot::hardware->register_callback<CAN0>(0x205, [&](const auto &frame) { yaw_motor.unpack(frame); });
-        Robot::hardware->register_callback<CAN0>(0x206, [&](const auto &frame) { pitch_motor.unpack(frame); });
+        Robot::hardware->register_callback<CAN1>(0x205, [&](const auto &frame) { yaw_motor.unpack(frame); });
+        Robot::hardware->register_callback<CAN1>(0x206, [&](const auto &frame) { pitch_motor.unpack(frame); });
     }
 
     void Gimbal::start_init_loop() {
@@ -26,13 +26,18 @@ namespace Gimbal
     }
 
     void Gimbal::init_loop() {
-        LOG_INFO("init loop %f, %f, %f, %u, %u\n", robot_set->ins_roll, robot_set->ins_pitch, robot_set->ins_yaw,
-                 yaw_motor.motor_measure.ecd, pitch_motor.motor_measure.ecd);
+        LOG_INFO(
+            "init loop %f, %f, %f, %u, %u\n",
+            robot_set->ins_roll,
+            robot_set->ins_pitch,
+            robot_set->ins_yaw,
+            yaw_motor.motor_measure.ecd,
+            pitch_motor.motor_measure.ecd);
         update_data();
         init_yaw_set += UserLib::rad_format(0.f - robot_set->yaw_relative) * Config::GIMBAL_INIT_YAW_SPEED;
         init_pitch_set += UserLib::rad_format(0.f - robot_set->ins_pitch) * Config::GIMBAL_INIT_PITCH_SPEED;
 
-        yaw_relative_pid.calc(robot_set->yaw_relative, init_yaw_set);
+        yaw_relative_pid.calc(robot_set->yaw_relative, 0);
         yaw_motor.speed_set = yaw_relative_pid.out;
         yaw_motor.pid_ctrler.calc(yaw_gyro, yaw_motor.speed_set);
         yaw_motor.give_current = (int16_t)yaw_motor.pid_ctrler.out;
@@ -55,7 +60,7 @@ namespace Gimbal
             init_stop_times = 0;
         }
         inited = init_stop_times >= Config::GIMBAL_INIT_STOP_TIME;
-        Robot::hardware->send<CAN0>(Hardware::get_frame(0x1FF, yaw_motor, pitch_motor));
+        Robot::hardware->send<CAN1>(Hardware::get_frame(0x1FF, yaw_motor, pitch_motor));
     }
 
     void Gimbal::control_loop() {
@@ -63,6 +68,23 @@ namespace Gimbal
         if (no_force) {
             yaw_motor.give_current = 0.f;
             pitch_motor.give_current = 0.f;
+        } else if (!robot_set->is_aiming) {
+            static fp32 search_angle = robot_set->yaw_relative;
+            static fp32 search_pitch = robot_set->ins_pitch;
+            static fp32 time_step = 0.f;
+            yaw_absolute_pid.calc(robot_set->ins_roll, search_angle);
+            yaw_motor.speed_set = yaw_absolute_pid.out;
+            yaw_motor.pid_ctrler.calc(yaw_gyro, yaw_motor.speed_set);
+            yaw_motor.give_current = (int16_t)yaw_motor.pid_ctrler.out;
+
+            pitch_absolute_pid.calc(robot_set->ins_pitch, search_pitch);
+            pitch_motor.speed_set = pitch_absolute_pid.out;
+            pitch_motor.pid_ctrler.calc(pitch_gyro, pitch_motor.speed_set);
+            pitch_motor.give_current = -(int16_t)pitch_motor.pid_ctrler.out;
+
+            search_angle = UserLib::rad_format(robot_set->ins_roll - 0.2);
+            search_pitch = std::sin(time_step) * M_PIf / 12;
+            time_step += M_PIf / 360;
         } else {
             yaw_absolute_pid.calc(robot_set->ins_roll, yaw_set);
             yaw_motor.speed_set = yaw_absolute_pid.out;
@@ -74,7 +96,7 @@ namespace Gimbal
             pitch_motor.pid_ctrler.calc(pitch_gyro, pitch_motor.speed_set);
             pitch_motor.give_current = -(int16_t)pitch_motor.pid_ctrler.out;
         }
-        Robot::hardware->send<CAN0>(Hardware::get_frame(0x1FF, yaw_motor, pitch_motor));
+        Robot::hardware->send<CAN1>(Hardware::get_frame(0x1FF, yaw_motor, pitch_motor));
     }
 
     void Gimbal::update_data() {
