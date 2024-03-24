ee3325566fa993ecff88e55a9f709db46479d67a
RhineGST <GST49151709884@outlook.com>
2024-03-25 07:47:37 +0800

    test commit

--- src/main.cc
+++ src/main.cc
@@ -11,14 +11,25 @@ int main(int argc, char **argv) {
 
     robot.load_hardware();
     robot.start_init();
-    robot.init_join();
-    LOG_INFO("finish init\n");
-
+//    robot.init_join();
+//    LOG_INFO("finish init\n");
+//
     robot.robot_set->mode = Types::ROBOT_MODE::ROBOT_FOLLOW_GIMBAL;
-    robot.robot_set->yaw_set = robot.robot_set->ins_roll;
-
+//    robot.robot_set->yaw_set = robot.robot_set->ins_roll;
+//
     robot.start();
-    robot.join();
+
+    while (true) {
+        uint32_t type;
+        std::cin >> type;
+        if(type == 1) {
+            robot.robot_set->friction_open ^= 1;
+        }
+        if(type == 2) {
+            robot.robot_set->shoot_open ^= 1;
+        }
+    }
+//    robot.join();
 
     return 0;
 }
